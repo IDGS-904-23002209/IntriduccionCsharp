9066e7d40a085cb90e448bd43e200856bb662623
Jimena Oropeza Cruces <oropezajim11@gmail.com>
2026-05-06 20:54:57 -0600

    Modelos 06/05/2026

---
+++ IntriduccionCsharp/Models/Practica2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntriduccionCsharp.Models
+{
+    /*
+     * class [nombre]{
+     * [atributos o variables]
+     * [metodos o funciones]
+     */
+    class Practica2
+    {
+        string nombre;
+        private int edad;
+        protected string carrera;
+        
+        public void IngresarDatos() {
+            Console.WriteLine("Ingresa tu nombre: ");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Ingresa tu edad: ");
+            edad = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingresa tu carrera: ");
+            carrera = Console.ReadLine();
+
+        }
+}
---
+++ IntriduccionCsharp/Services/PeliculaServices.cs
@@ -0,0 +1,31 @@
+﻿using IntriduccionCsharp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace IntriduccionCsharp.Services
+{
+    public class PeliculaServices
+    {
+        public List<Pelicula> ObtenerPelicula()
+        {
+            var pelicula1 = new Pelicula()
+            {
+                Titulo = "Superman",
+                Duracion = 120,
+                Pais = "USA",
+                Publicacion = new DateTime(2023, 12, 5)
+            };
+            var pelicula2 = new Pelicula()
+            {
+                Titulo = "Superman",
+                Duracion = 111,
+                Pais = "USA",
+                Publicacion = new DateTime(2015, 05, 5)
+            };
+            return new List<Pelicula>{ pelicula1, pelicula2 };
+        }
+    }
+}
