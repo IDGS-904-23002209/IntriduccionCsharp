ce7bd74913fff974ea1d67e8996a0b095f7a334d
Jimena Oropeza Cruces <oropezajim11@gmail.com>
2026-05-13 20:28:49 -0600

    Suma AporB 13/05/2026

---
+++ IntriduccionCsharp/Controllers/OperacionAporBController.cs
@@ -0,0 +1,20 @@
+﻿using IntriduccionCsharp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IntriduccionCsharp.Controllers
+{
+    public class OperacionAporBController : Controller
+    {
+        public ActionResult SumaAporB(OperacionAporB Suma)
+        {
+            Suma.Operacion();
+
+            return View(Suma);
+        }
+    }
+}
+
---
+++ IntriduccionCsharp/Models/OperacionAporB.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IntriduccionCsharp.Models
+{
+    public class OperacionAporB
+    {
+        public double A { get; set; }
+        public double B { get; set; }
+        public double Resp { get; set; }
+        public string RestSuma { get; set; }
+
+        public void Operacion() 
+        {
+            for (int i = 1; i <= B; i++)
+            {
+
+                this.Resp += (double)A ;
+                this.RestSuma += Convert.ToString(A);
+                if(i != 20)
+                {
+                    this.RestSuma += " + ";
+                }
+            }
+        }
+
+    }
+}
